c0cf2d29694fd5d811fa1bffd7ed1f715ed1f5d0
Rohith055 <153158314+Githubusages@users.noreply.github.com>
2024-09-17 12:06:20 +0530

    Basics

---
+++ Break Stmt.py
@@ -0,0 +1,10 @@
+x =(int(input("Enter the number:")))
+av = 15
+i = 1
+while i<=x:
+    # if you want to stop it according to your limit.
+    if i>av:
+        break;# breaks the loop
+    print("CANDIES")
+    i += 1
+print("Available in shortly")
