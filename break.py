eb9522ebbd73473042b3275c1d43d435a1597bb6
Rohith055 <153158314+Githubusages@users.noreply.github.com>
2024-09-17 12:12:42 +0530

    Basics

---
+++ break.py
@@ -0,0 +1,9 @@
+for i in range (1,10):
+    if i%2==0:
+        pass# it just ignores the current loop and proceeds to the next loop.
+        # continue#It skips the further execution and continues the remaining.
+# Here I wna to skip the evens
+    else:
+        print (i)
+
+print("ends here")
