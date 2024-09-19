7951ec1708c6376dc9fff13fb24844717f3fbdc2
Rohith055 <153158314+Githubusages@users.noreply.github.com>
2024-09-19 19:50:39 +0530

    Basics

--- two-dim-arr.py
+++ two-dim-arr.py
@@ -19,3 +19,25 @@ print(arr3)
 print(arr1.shape)
 # It gives the size of the array.
 print(arr1.size)
+
+# Converting a array into a matrix
+# ar1 = array([
+#     [1,2,3,4,5]
+#     [6,7,8,9,10,11]
+# ])
+# m = matrix(ar1)
+
+# Using the Matrix we can perform a many operations
+m = matrix('1 2 3; 4 5 6; 7 8 9')
+print(m)
+# Prints the diagonal element of a matrix
+print(diagonal(m))
+# Printing the min of a array
+print(min(m))
+#  Printing the max of a array
+print(max(m))
+
+m1 = matrix('1 2 3; 4 5 6; 7 8 9')
+m2 = matrix('1 2 3; 4 5 6; 7 8 9')
+m3 = m1 * m2;
+print(m3)
