7a4345156559a9bb45deddb83922dca906bad27b
Rohith055 <153158314+Githubusages@users.noreply.github.com>
2024-10-12 10:47:26 +0530

    Lambda func in Py

---
+++ Basics/lambda1.py
@@ -0,0 +1,10 @@
+# Here we are going to create a lambda func
+# In python The functions are objects
+# Here we can able to convert the funcs as a Object 
+
+f  = lambda a,b : a+b
+# Here the lambda function should contains any number of variable's should contains one line expression
+
+# The values of a&b is passed in this 
+result = f(5,6)
+print(f)
