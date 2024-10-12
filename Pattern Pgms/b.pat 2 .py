b9911f40242437af9ee54f7ab0e65b20d327e0c3
Rohith055 <153158314+Githubusages@users.noreply.github.com>
2024-10-12 12:49:23 +0530

    Increasing Stars

---
+++ Pattern Pgms/b.pat 2 .py
@@ -0,0 +1,7 @@
+n = (int(input("Enter the number :")))
+# Getting the input from the user 
+# Printing the Right angled Triange pattern
+for i in range(n):
+    for j in range(i+1):
+        print("#",end=' ')
+    print()
