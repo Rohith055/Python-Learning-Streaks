f745895a76ba2edd6be495f943fbfe785b7501a2
Rohith055 <153158314+Githubusages@users.noreply.github.com>
2024-10-12 12:48:54 +0530

    Printing Squares

---
+++ Pattern Pgms/a.pat 1 .py
@@ -0,0 +1,10 @@
+n = (int(input("Enter the number :")))
+# Getting the input from the user 
+# Printing the Stars in the square format
+
+for i in range (n): 
+    # Ranges upto n rows
+    for j in range(n):
+    # Ranges upto n cols
+        print("*",end=" ")
+    print()
