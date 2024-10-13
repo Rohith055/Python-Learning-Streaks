65b6f88fcd78f3b3aa27a9207514159e533194a3
Rohith055 <brohith519@gmail.com>
2024-10-13 09:58:11 +0530

    Modified Inverted Hill Stars

--- Pattern Pgms/g.pat 7.py
+++ Pattern Pgms/g.pat 7.py
@@ -4,11 +4,14 @@ Here we are going to print the inverted hill form of triangle
 
 '''
 for i in range(n):
-    for j in range(i+1):
+    for j in range(i):
+        # Increasing Order of the Space 
         print(" ",end=" ")
-    for j in range(i):     
+    for j in range(i,n):     
+        # Decreasing Order of the Stars
         print("+",end=" ") 
-    for j in range(i+1):
+    for j in range(i,n-1):
+        # Decreasing Order of the Stars in repect to n-1 to achive the hill like structure.
         print("+",end=" ")
   
     print()
