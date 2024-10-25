51865f730ae3ab321fd72492376bdfc4a0d4c677
Rohith055 <brohith519@gmail.com>
2024-10-25 19:09:57 +0530

    Modules in Py

---
+++ Basics/Modules.py
@@ -0,0 +1,22 @@
+
+from calcop import *
+# from calcop import add
+# import calcop
+
+
+
+''' 
+Here we're going to see about the Modules in python.
+Modules are nothing but a separation of a large programs into smaller parts.
+we can create a separate module and call it here using the import stmt.
+
+'''
+
+a = 10
+b = 15
+
+c = add(a,b)
+
+print(c)
+
+# Like this we can able to perform the modules
