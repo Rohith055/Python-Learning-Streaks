0548daa771d0866a04348d76629aa3a0f644ba81
Rohith055 <brohith519@gmail.com>
2024-11-07 21:54:23 +0530

    Constructors

---
+++ OOPS/Constructors.py
@@ -0,0 +1,11 @@
+class comp:
+    def __init__(self):
+        self.name = 'R O H I'
+        self.age = 20
+
+c1 = comp()
+
+print(c1.name)
+print(c1.age)
+# Size of the object depends on the number of variable.
+
