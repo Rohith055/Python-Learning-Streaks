563ba7d95eaaa99405085703a4ca06d74bc97e94
Rohith055 <brohith519@gmail.com>
2024-11-27 21:24:03 +0530

    Comments

---
+++ OOPS/Comments.py
@@ -0,0 +1,8 @@
+# Here we are going to see about the Comments in python
+
+# Here '#' Represents the Single Line comment for refering the Code lines with it
+
+'''
+This is been used for multiline Documentary 
+Not that much suitable for the Comments in Code.
+'''
