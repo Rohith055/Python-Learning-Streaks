a926e294708998f579de947b47e85fb22edadbd4
Rohith055 <brohith519@gmail.com>
2024-11-28 20:06:36 +0530

    File Handling

--- OOPS/File Handling/File Handling.py
+++ OOPS/File Handling/File Handling.py
@@ -4,24 +4,24 @@
 
 # Opening a File
 
-# file_p = 'D:\Basic(Py)\OOPS\\File Handling\Mydata'         # It specifies the path of the file
-# f = open(file_p,'r')                        # Opening the File in Read mode.
+file_p = 'D:\Basic(Py)\OOPS\\File Handling\Mydata'         # It specifies the path of the file
+f = open(file_p,'r')                        # Opening the File in Read mode.
 
-# print(f.read())                             # Printing the File.
-# print(f.readline())                         # It fetches the value of the first line only, For printing this next line the print statement is again repeated 
-# print(f.readline())                         # It will prints the next line
-# print(f.readline(5))                          # Here it will prints the First five characters only.
+print(f.read())                             # Printing the File.
+print(f.readline())                         # It fetches the value of the first line only, For printing this next line the print statement is again repeated 
+print(f.readline())                         # It will prints the next line
+print(f.readline(5))                          # Here it will prints the First five characters only.
 
 # Writing a File 
 
-# f1 = open('sample','a')                       # Here the write func is for only to write and not for saving the Process.
+f1 = open('sample','a')                       # Here the write func is for only to write and not for saving the Process.
                                               # To sava the file we need to use the append which is 'a'.
-# f1.write(" This is a Sample File. Created Using the Write Command.") 
+f1.write(" This is a Sample File. Created Using the Write Command.") 
 
 # Printing the ever data in the file one by one
 
-# for data in f:                                # Here the File in read mode can only be viewed
-    # print(data)
+for data in f:                                # Here the File in read mode can only be viewed
+    print(data)
 
 # Opening a Img File.
 f_o = 'D:\Basic(Py)\OOPS\\File Handling\R O H I (1).png'
